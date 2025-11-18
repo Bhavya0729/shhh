e231a06b7e2be58351f9adc141cb53199c49bd9d
Bhavya0729 <rbhavya672@gmail.com>
2025-11-18 16:21:50 +0530

    Update python.groovy

--- vars/python.groovy
+++ vars/python.groovy
@@ -4,24 +4,5 @@ def gitdownload(repo)
 }
 def build()
 {
-  sh "mvn clean package"
-}
-def deployment(jobname,ip)
-{
- sh """
-scp /var/lib/jenkins/workspace/${jobname}/Helloworld.java ubuntu@${ip}:/home/ubuntu/ten
-"""
-}
-def testing(jobname)
-{
-sh """
-    javac /var/lib/jenkins/workspace/${jobname}/Helloworld.java
-    java -cp /var/lib/jenkins/workspace/${jobname} Helloworld > /var/lib/jenkins/workspace/${jobname}/test_output.txt
-"""
-}
-def release(jobname,ip)
-{
- sh """
-scp /var/lib/jenkins/workspace/${jobname}/Helloworld.java ubuntu@${ip}:/home/ubuntu/ten
-"""
+  sh "python3 python.py"
 }
